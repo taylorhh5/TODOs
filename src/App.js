9cf42096744d188fb62e36fbd5b3f78f0c8c68f1
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-06-24 16:15:53 -0400

    Todo class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //for class component
+  constructor() {
+    super();
+
+    //state
+    this.state = {
+      item: "",
+      items: [],
+    };
+
+    //binding
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submitItem = this.submitItem.bind(this);
+  }
+
+  //handlechange
+  handleInputChange(event) {
+    // let name = event.target.name;
+    // let value = event.target.value;
+    // //setting state to whats in form
+    // this.setState({
+    //   [name]: value,
+    // });
+
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  //add new item
+  submitItem() {
+    let banana1 = this.state.items;
+    let banana2 = this.state.item;
+
+    //pushing new item into items array
+    banana1.push(banana2);
+
+    //new state could just be banana1?
+    this.setState({ banana1: banana1 });
+  }
+
+  //render for class component
+  render() {
+    return (
+      <div className="App">
+        <h1>TODO List App</h1>
+        {/* <h1>{this.state.items}</h1> */}
+        <input type="text" name="item" onChange={this.handleInputChange} />
+
+        <button onClick={this.submitItem}>Submit</button>
+
+        {this.state.items.map((item) => {
+          return <p>{item}</p>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
